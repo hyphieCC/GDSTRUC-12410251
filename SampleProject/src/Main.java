b87462a13b588d739d967189c934c7a24b89a5f6
Charles <charles.castillo@benilde.edu.ph>
2025-09-10 09:09:56 +0800

    Testing git

---
+++ SampleProject/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner reader = new Scanner(System.in);
+        String name = reader.next();
+        System.out.println("Hello " + name);
+    }
+}
